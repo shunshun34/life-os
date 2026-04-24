ae0a5d0db4f47353c1a92d2b9177336383984bfd
shunshun34 <shunshun3574@gmail.com>
2026-04-24 23:09:20 +0900

    PWA登録修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,9 @@ export const viewport: Viewport = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="ja">
       <body>
